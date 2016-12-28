7933788348a9f8fb578e47ba2b08ec9bdfb21d49
Alexander Lakhin <exclusion@gmail.com>
2016-12-28 18:07:35 +0300

    Remove redundant modification of pg_hba.conf for appdb

--- vagrant_scripts/appdb_bootstrap.sh
+++ vagrant_scripts/appdb_bootstrap.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-echo "host    all             all             10.0.0.14/32            md5" >> /etc/postgresql/9.3/main/pg_hba.conf
-echo "host    all             all             10.0.0.24/32            md5" >> /etc/postgresql/9.3/main/pg_hba.conf
-service postgresql reload
-
 # initialize the sqlfiddle central database, which has all sqlfiddle-specific data structures
 createdb -U postgres -E UTF8 sqlfiddle
 psql -U postgres sqlfiddle < /vagrant/src/main/resources/db/sqlfiddle/schema.sql

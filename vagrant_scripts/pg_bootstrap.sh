ed3aace3c287ab6f7432768f664625e13d1928f6
Alexander Lakhin <exclusion@gmail.com>
2016-12-28 17:00:31 +0300

    Add pg_bootstrap script

---
+++ vagrant_scripts/pg_bootstrap.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+
+pgver=$1
+locale=$2
+if [ ! -z $locale ]; then
+    locale-gen $locale
+    localectl set-locale LANG=$locale
+    export LANG=$locale
+fi
+# create a 512mb swapfile
+dd if=/dev/zero of=/swapfile1 bs=1024 count=524288
+chown root:root /swapfile1
+chmod 0600 /swapfile1
+mkswap /swapfile1
+swapon /swapfile1
+echo "/swapfile1 none swap sw 0 0" >> /etc/fstab
+
+echo "deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+apt-get --yes update
+apt-get --yes upgrade
+apt-get --yes install postgresql-$pgver postgresql-contrib-$pgver
+
+pg_dropcluster --stop $pgver main
+echo "listen_addresses = '*'" >> /etc/postgresql-common/createcluster.conf
+echo "max_connections = 500" >> /etc/postgresql-common/createcluster.conf
+
+pg_createcluster --start $pgver main -- --auth-local=trust
+echo "host    all             all             10.0.0.14/32            md5" >> /etc/postgresql/$pgver/main/pg_hba.conf
+echo "host    all             all             10.0.0.24/32            md5" >> /etc/postgresql/$pgver/main/pg_hba.conf
+service postgresql reload
+
+echo "alter user postgres with password 'password';" | psql -U postgres
+iptables -A INPUT -p tcp --dport 5432 -j ACCEPT
+
+# initialize the template database, used by fiddle databases running in this env
+psql -U postgres postgres < /vagrant/src/main/resources/db/postgresql/initial_setup.sql
+psql -U postgres db_template < /vagrant/src/main/resources/db/postgresql/db_template.sql

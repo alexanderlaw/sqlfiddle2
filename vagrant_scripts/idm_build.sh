9b15adb916ed492359f821c77c8b2120552a50fa
Alexander Lakhin <exclusion@gmail.com>
2016-12-28 20:22:48 +0300

    Rid of jTDS for SQL, odbc for Oracle

--- vagrant_scripts/idm_build.sh
+++ vagrant_scripts/idm_build.sh
@@ -5,30 +5,9 @@ apt-get --yes install maven npm nodejs-legacy
 
 npm install -g grunt-cli
 
-cd ~
-wget -q http://dl.dropbox.com/u/2590603/bnd/biz.aQute.bnd.jar
-
-# OSGi wrap the jTDS driver for SQL Server
-wget -q http://central.maven.org/maven2/net/sourceforge/jtds/jtds/1.3.1/jtds-1.3.1.jar
-java -jar ~/biz.aQute.bnd.jar wrap -properties /vagrant/vagrant_scripts/jtds.bnd ./jtds-1.3.1.jar
-mv /vagrant/vagrant_scripts/jtds-1.3.1.bar ~/jtds-1.3.1.jar
-mvn install:install-file -DgroupId=net.sourceforge.jtds -DartifactId=jtds -Dversion=1.3.1 -Dpackaging=jar -Dfile=./jtds-1.3.1.jar
-
 cd /vagrant
 mvn -B clean install
 npm install
-
-
-# If you want to enable Oracle support, manually download ojdbc6.jar and put it in the root folder (up one level from here)
-# Download it from here: http://www.oracle.com/technetwork/database/enterprise-edition/jdbc-112010-090769.html
-if [ -e "/vagrant/ojdbc6.jar" ]
-then
-    java -jar ~/biz.aQute.bnd.jar wrap -properties /vagrant/vagrant_scripts/ojdbc6.bnd /vagrant/ojdbc6.jar
-    mv /vagrant/vagrant_scripts/ojdbc6.bar ojdbc6.jar
-    mvn install:install-file -DgroupId=com.oracle -DartifactId=ojdbc6 -Dversion=11.2.0.4 -Dpackaging=jar -Dfile=./ojdbc6.jar
-    cp ./ojdbc6.jar /vagrant/target/sqlfiddle/bundle
-fi
-
 
 cd target/sqlfiddle/bin
 ./create-openidm-rc.sh
